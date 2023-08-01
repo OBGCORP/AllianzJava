9156af73ab6c3f4fee72ebd9da3372b2b911fe2c
Onur Berk Gunes <obgunes99@gmail.com>
2023-08-01 12:24:05 +0300

    Spring 01.08.2023

--- example/src/main/java/com/allianz/example/service/PersonService.java
+++ example/src/main/java/com/allianz/example/service/PersonService.java
@@ -1,10 +1,14 @@
 package com.allianz.example.service;
 
 import com.allianz.example.model.Person;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PersonService {
+
+    @Value("${gizem:25}")
+    int value;
 
     public Person createPerson(String name, String surname, String tc, int birthYear) {
         Person person = new Person();
@@ -12,6 +16,7 @@ public class PersonService {
         person.setName(name);
         person.setSurname(surname);
         person.setBirthYear(birthYear);
+        System.out.println(value);
         return person;
     }
 }
