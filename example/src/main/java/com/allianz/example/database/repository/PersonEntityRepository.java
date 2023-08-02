67efdd007de15231156677fb8b5b0aa817fda55d
Onur Berk Gunes <obgunes99@gmail.com>
2023-08-02 11:02:41 +0300

    Spring 02.08.2023

---
+++ example/src/main/java/com/allianz/example/database/repository/PersonEntityRepository.java
@@ -0,0 +1,15 @@
+package com.allianz.example.database.repository;
+
+import com.allianz.example.database.entity.PersonEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PersonEntityRepository extends JpaRepository<PersonEntity, Long> {
+
+    List<PersonEntity> findAllByNameStartingWith(String key);
+
+    List<PersonEntity> findAllByNameContainsIgnoreCase(String key);
+}
